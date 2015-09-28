b4897d9df36b43457c07cf5dced6e93e2357a319
Danil Simutin <danzilll@inbox.ru>
2015-09-28 19:13:05 +0600

    MinMax feature

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -13,18 +14,42 @@ public:
         _size = size;
         pointer = new int [size];
         for (int i = 0; i < size; i++) {
-            pointer[i] = i;
+            pointer[i] = rand() % 100;
+        }
+    }
+    ~IntegerArray(){
+        cout << "Object destroyed!" << endl;
+    }
+    IntegerArray(int size){
+        _size = size;
+        pointer = new int [size];
+        for (int i = 0; i < size; i++) {
+            pointer[i] = rand() % 100;
         }
     }
     void PrintArray(){
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < _size; i++) {
             cout << pointer[i] << endl;
         }
+    }
+    void MinMax(){
+        int min = 101, max = 0;
+        for (int i = 0; i < _size; i++) {
+            if (pointer[i] < min) {
+                min = pointer[i];
+            }
+            if (pointer[i] > max) {
+                max = pointer[i];
+            }
+        }
+        cout << "Min: " << min << endl;
+        cout << "Max: " << max << endl;
     }
 };
 
 int main() {
-    IntegerArray test;
+    IntegerArray test(15);
     test.PrintArray();
+    test.MinMax();
     return 0;
 }
