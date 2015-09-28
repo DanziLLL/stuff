de890c7560a097f63aaac4fb41bc1b1f60f1bb90
Danil Simutin <danzilll@inbox.ru>
2015-09-28 18:29:59 +0600

    default constructor

--- main.cpp
+++ main.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+class IntegerArray
+{
+private:
+    int _size;
+    int *pointer;
+public:
+    IntegerArray(){
+        int size = 10;
+        _size = size;
+        pointer = new int [size];
+    }
+};
+
 int main() {
     cout << "Hello, World!" << endl;
     return 0;
