e1feb6c4d01e995e5f216d4add3115f1b96e25a8
Danil Simutin <danzilll@inbox.ru>
2015-09-28 18:41:28 +0600

    PrintArray feature

--- main.cpp
+++ main.cpp
@@ -12,10 +12,19 @@ public:
         int size = 10;
         _size = size;
         pointer = new int [size];
+        for (int i = 0; i < size; i++) {
+            pointer[i] = i;
+        }
+    }
+    void PrintArray(){
+        for (int i = 0; i < 10; i++) {
+            cout << pointer[i] << endl;
+        }
     }
 };
 
 int main() {
-    cout << "Hello, World!" << endl;
+    IntegerArray test;
+    test.PrintArray();
     return 0;
 }
