0ec271246d649b8b62d33bbe42264265f0ede3f5
Danil Simutin <danzilll@inbox.ru>
2015-11-23 13:10:40 +0600

    basic commit

--- main.cpp
+++ main.cpp
@@ -1,59 +1,19 @@
-#define pr << a << "+" << b << "=";
-#define NOTTEMPLATE
 #include <iostream>
+#include <fstream>
+#include <set>
 
 using namespace std;
 
-#ifndef NOTTEMPLATE
-template <typename Type>
-    int smth(int a, Type b){
-        cout << "Int-templated pair acquired: " pr;
-        return a+b;
-    }
-#endif
-
-#ifdef NOTTEMPLATE
-int smth(int a, int b);
-int smth(int a, float b);
-int smth(int a, char b);
-int smth(int a, long b);
-int smth(int a, double b);
-#endif
-
 int main(){
-    int dummy1 = 10;
-    float dummy2 = 13.0;
-    char dummy3 = 'd';
-    long dummy4 = 20.0;
-    double dummy5 = 31.0;
-    cout << smth(dummy1, dummy1) << endl;
-    cout << smth(dummy1, dummy2) << endl;
-    cout << smth(dummy1, dummy3) << endl;
-    cout << smth(dummy1, dummy4) << endl;
-    cout << smth(dummy1, dummy5) << endl;
+    fstream list;
+    list.open("1.txt", fstream::in | fstream::out);
+    string nextword;
+    set<string> words;
+    while (!list.eof()){
+        list >> nextword;
+        words.insert(nextword);
+    }
+    int set_size = words.size();
+    cout << set_size;
     return 0;
 }
-
-#ifdef NOTTEMPLATE
-int smth(int a, int b){
-    cout << "Int-int pair acquired: " pr;
-    return a+b;
-}
-int smth(int a, float b){
-    cout << "Int-float pair acquired: " pr;
-    return a+b;
-}
-int smth(int a, char b){
-    cout << "Int-char pair acquired: " pr;
-    return a+b;
-}
-int smth(int a, long b){
-    cout << "Int-long pair acquired: " pr;
-    return a+b;
-}
-
-int smth(int a, double b){
-    cout << "Int-double pair acquired: " pr;
-    return a+b;
-}
-#endif
