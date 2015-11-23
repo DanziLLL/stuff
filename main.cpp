f67a09ba3804294adf45d218e3922c05a795a9e7
Danil Simutin <danzilll@inbox.ru>
2015-11-23 13:38:40 +0600

    basic commit

--- main.cpp
+++ main.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 #include <fstream>
 #include <set>
+#include <bits/stl_vector.h>
 
 using namespace std;
 
 int main(){
     fstream list;
-    list.open("1.txt", fstream::out);
+    list.open("1.txt",fstream::in | fstream::out);
     string nextword;
     set<string> words;
-    getline(list, nextword, '\n');
-    cout << nextword;
     while (!list.eof()){
         list >> nextword;
         words.insert(nextword);
     }
-    int set_size = words.size();
-    cout << set_size;
+    list.close();
+    vector<string> secondfile;
+    fstream text;
+    text.open("2.txt", fstream::in | fstream::out);
+    while (!text.eof()){
+        text >> nextword;
+        secondfile.insert(secondfile.end(), nextword);
+    }
+    cout << secondfile.size();
     return 0;
 }
